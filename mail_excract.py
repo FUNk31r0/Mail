030289cf06ec13345f68a73a80038b3689a80785
FUNk31r0 <cardoso.natanael2012@gmail.com>
2017-04-01 05:30:01 -0300

    Update mail_excract.py

--- mail_excract.py
+++ mail_excract.py
@@ -9,7 +9,7 @@ _site_ = sys.argv[1]
 _saida_ = sys.argv[2]
 _file_ = open(_saida_, "w")
 t = Terminal()
-r = requests.get(_site_)
+r = requests.get(_site_, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'})
 conteudo = r.content
 _file_ = open(_saida_, "w")
 _filtro_ = re.findall(r'[\w\.-]+@[\w\.-]+', conteudo)
